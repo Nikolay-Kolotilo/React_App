e10fb47e5755d477770a6228ec9784c9b58748c8
Nikolay_Kolotilo <3dnikik00@gmail.com>
2023-02-03 13:19:54 +0200

    Lec_125__React_Create a new project

---
+++ my-app/src/components/search-panel/search-panel.js
@@ -0,0 +1,14 @@
+import './search-panel.css';
+
+const SearchPanal = () => {
+        return (
+            <input
+                type="text"
+                className='form-control search-input'
+                placeholder='Найти сотрудника'/>
+            )
+}
+
+
+
+export default SearchPanal
